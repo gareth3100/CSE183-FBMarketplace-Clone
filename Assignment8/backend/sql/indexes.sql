771443480af2ec1468c4f61eac28c19f2f636a96
James Nguyen <jameslittlefoot@yahoo.com>
2021-11-28 20:51:36 -0800

    SQL table and data insertion

--- Assignment8/backend/sql/indexes.sql
+++ Assignment8/backend/sql/indexes.sql
@@ -1,1 +1,10 @@
 -- Index Your Tables Here --
+insert into AccountUser(AccountUserId,AccountId,FirstName,LastName,LoginEmail,RecoveryPhone,PasswordHash,CanLogin,LockedDateTime,ResetToken) 
+values (
+	'00000000-0000-0000-0000-000000000001', --@AccountUserId,
+	'Test',
+	'User',
+	'sysadmin@abhs.com', --@LoginEmail,
+	'123-123-1234', --@RecoveryPhone,
+	'$2a$10$wvbrVP4xUn/ZLt0gIEliu.7k9VnOi2IcDoKfSQ8EY1GMRSw.17Q1O', --@PasswordHash,
+);
--- Assignment8/backend/sql/schema.sql
+++ Assignment8/backend/sql/schema.sql
@@ -3,3 +3,26 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+create table if not exists User (
+	UserId uuid NOT NULL  
+	constraint "pk-AccountUser" primary key,
+	FirstName text NOT NULL,
+	LastName text NOT NULL,
+	LoginEmail text NOT NULL  ,
+	RecoveryPhone text  ,
+	PasswordHash text NOT NULL
+);
+
+create table if not exists Category(
+	Name text NOT NULL,
+	AssociatedFilters text NOT NULL,
+	Subcategories text NOT NULL
+);
+
+create table if not exists Listing(
+	UserId NOT NULL,
+	CreationTime datetime NOT NULL,
+	Content jsonb NOT NULL,
+	Replies text NOT NULL
+);
+
