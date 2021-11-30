c7d515999881b87b92002413ec353779c12eb6f3
James Nguyen <jameslittlefoot@yahoo.com>
2021-11-29 22:51:11 -0800

    Database insertion recommit

--- Assignment8/backend/sql/schema.sql
+++ Assignment8/backend/sql/schema.sql
@@ -3,3 +3,24 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+create table if not exists AccountUser(
+	AccountUserId uuid NOT NULL,  
+	FirstName text NOT NULL,
+	LastName text NOT NULL,
+	LoginEmail text NOT NULL,
+	RecoveryPhone text,
+	PasswordHash text NOT NULL
+);
+
+create table if not exists Category(
+	Name text NOT NULL,
+	AssociatedFilters text NOT NULL,
+	Subcategories text NOT NULL
+);
+
+create table if not exists Listing(
+	UserId uuid NOT NULL,
+	CreationTime TIMESTAMP WITH TIME ZONE NOT NULL,
+	Content jsonb NOT NULL,
+	Replies text NOT NULL
+);
