fc6fce4715e1082edcc746a2121f2c47c9918c58
Juan Lee <69443239+JUANie10@users.noreply.github.com>
2021-12-02 04:51:36 -0800

    more States

--- Assignment8/frontend/src/components/App.js
+++ Assignment8/frontend/src/components/App.js
@@ -13,6 +13,8 @@ export const WorkspaceContext = createContext();
  */
 function App() {
   const [currentCategory, setCurrentCategory] = React.useState('');
+  const [currentCategoryData, setCurrentCategoryData] = React.useState('');
+  const [currentSubcategory, setCurrentSubcategory] = React.useState('');
   const [openLocation, setOpenLocation] = React.useState(false);
   const [openFilter, setOpenFilter] = React.useState(false);
   const [loggedIn, setLoggedIn] = React.useState(false);
@@ -25,6 +27,8 @@ function App() {
           openLocationS: [openLocation, setOpenLocation],
           loggedInS: [loggedIn, setLoggedIn],
           openFilterS: [openFilter, setOpenFilter],
+          currentSubcategoryS: [currentSubcategory, setCurrentSubcategory],
+          currentCategoryDataS: [currentCategoryData, setCurrentCategoryData],
         }}
       >
         <Switch>
