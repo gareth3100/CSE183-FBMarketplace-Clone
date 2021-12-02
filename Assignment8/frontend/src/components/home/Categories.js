295d6c7397ebb6e64d1518eae5d3208c689aa9d3
gareth3100 <garethsamadhana@gmail.com>
2021-12-01 22:33:41 -0800

    wq wq

--- Assignment8/frontend/src/components/home/Categories.js
+++ Assignment8/frontend/src/components/home/Categories.js
@@ -12,8 +12,6 @@ import Select from '@mui/material/Select';
 
 import {WorkspaceContext} from '../Home';
 
-const db = require('./src/db');
-
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   category: {
