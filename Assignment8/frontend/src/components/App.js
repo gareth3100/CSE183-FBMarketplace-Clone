46ab055585d8c4923a6faaeff3e755e2bb63d101
James Nguyen <jameslittlefoot@yahoo.com>
2021-12-03 04:43:10 -0800

    Display template to show user a certain listing

--- Assignment8/frontend/src/components/App.js
+++ Assignment8/frontend/src/components/App.js
@@ -4,6 +4,7 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Home from './Home';
 import {createContext} from 'react';
+import Display from './Display';
 
 export const WorkspaceContext = createContext();
 /**
@@ -51,6 +52,9 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignUp/>
+          </Route>
+          <Route path="/display">
+            <Display/>
           </Route>
         </Switch>
       </WorkspaceContext.Provider>
